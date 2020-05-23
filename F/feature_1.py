7f65e0ce07558374452dea4c0c386bb6be9e925b
Lutfee Deemae <lutfeedeemae@users.noreply.github.com>
2020-05-24 03:53:27 +0700

    f1

--- F/feature_1.py
+++ F/feature_1.py
@@ -5,7 +5,7 @@ def feature_1():
     try:
         get = request.get_json()
         # down here for calling north's functions
-        tempt=jsonify({'return':get['store_id']+get['years']+get['months']})    #Example, in this feature we have 3 variables that sended from yodjung
+        
+        return jsonify({'return':get['store_id']+get['years']+get['months']})
     except:
-        print("An error or exception occurred")     # this is whre I return JSON back to Yoodjung
-    return tempt
+        return "An error or exception occurred" 
