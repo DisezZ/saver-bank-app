ed4d60392f39954a5348e54663877a23ae4b271f
Lutfee Deemae <lutfeedeemae@users.noreply.github.com>
2020-05-23 23:56:26 +0700

    little update about index page

--- F/index.py
+++ F/index.py
@@ -3,5 +3,14 @@ def index():
 <h2>This API have 4 pages now<h2><br>
 <h3>1.This index Pages, path = /<h3><br>
 <h3>2.register API, path = /register<h3><br>
+<p>
+JSON key parameter = 'username','password','email','first_name','last_name'<br>return parameter = 'message','status'
+<p><br><hr><br>
 <h3>3.login API, path = /login<h3><br>
-<h3>4.1st Feature API, path = /feature_1<h3>'''
+<p>
+JSON key parameter = 'username','password'<br>return parameter = 'message','status'
+<p><br><hr><br>
+<h3>4. 1st Feature API, path = /feature_1<h3>
+<p>
+JSON key parameter = 'store_id','years','months'<br>return parameter = 'electric_prediction'
+<p><br><hr><br>'''
